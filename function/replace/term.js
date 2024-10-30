e430781a8133f151546e1e3b70dc082772128e67
Runwill <1798273016@qq.com>
2024-10-31 00:38:50 +0800

    分号

--- function/replace/term.js
+++ function/replace/term.js
@@ -101,7 +101,7 @@ function textReplace(path,mode) {
 }
 $(function () {
     $(document).ready(function () {
-        $(document).foundation();
+        $(document).foundation()
     })
     textReplace('base/term/fixed.json',1)
     textReplace('base/term/dynamic.json',1)
