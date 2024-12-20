ffa3b8a78c394b82df609e8ea3a7fc7eeb18c47a
Runwill <1798273016@qq.com>
2024-12-21 00:50:08 +0800

    term函数变量名简化

--- function/replace/term.js
+++ function/replace/term.js
@@ -4,7 +4,7 @@ function textReplace(path,mode) {
             if (!term[i].part) {//不分段术语
                 document.querySelectorAll(term[i].en).forEach(//替换
                     element => {
-                        element.termPosition = i
+                        element.i = i
                         if (!element.classList.contains('irreplaceable') && !(term[i].irreplaceable)) {
                             if (!term[i].epithet) {
                                 if (term[i].cn) element.innerHTML = term[i].cn
@@ -22,7 +22,8 @@ function textReplace(path,mode) {
                                     $("#example-tabs").foundation('selectTab', 'panel_term', 1);
                                     document.querySelectorAll(".scroll").forEach(
                                         scroll => {
-                                            if (scroll.outerHTML.startsWith("<" + event.currentTarget.tagName.toLowerCase() + " ")) {
+                                            var target = event.currentTarget
+                                            if (scroll.outerHTML.startsWith("<" + target.tagName.toLowerCase() + " ")) {
                                                 if (!(scroll.classList.contains('fadeOnly'))) scroll.scrollIntoView({ behavior: "smooth" })
                                                 $(scroll).fadeTo(200, 0).fadeTo(1000, 1)
                                             }
@@ -36,15 +37,16 @@ function textReplace(path,mode) {
                 if(mode){
                     $(term[i].en).mouseover(//高亮
                         function (event) {
-                            $(this).css("background-color", term[event.currentTarget.termPosition].color)
-                            $(term[event.currentTarget.termPosition].en + ".scroll").css("background-color", term[event.currentTarget.termPosition].color)
-                            if($(this).hasClass('highlight') || term[event.currentTarget.termPosition].highlight){
+                            var target = event.currentTarget
+                            $(this).css("background-color", term[target.i].color)
+                            $(term[target.i].en + ".scroll").css("background-color", term[target.i].color)
+                            if($(this).hasClass('highlight') || term[target.i].highlight){
                                 if($(this).closest('pronounScope').length > 0){
-                                    $(this).closest('pronounScope').find(term[event.currentTarget.termPosition].en).each(function() {
+                                    $(this).closest('pronounScope').find(term[target.i].en).each(function() {
                                         $(this).css("background-color", "#ff00002f")
                                     })
                                 }else{
-                                    $(this).closest('padding').find(term[event.currentTarget.termPosition].en).each(function() {
+                                    $(this).closest('padding').find(term[target.i].en).each(function() {
                                         $(this).css("background-color", "#ff00002f")
                                     })
                                 }
@@ -53,15 +55,16 @@ function textReplace(path,mode) {
                     )
                     $(term[i].en).mouseout(//去高亮
                         function (event) {
+                            var target = event.currentTarget
                             $(this).css("background-color", "")
-                            $(term[event.currentTarget.termPosition].en + ".scroll").css("background-color", "")
-                            if($(this).hasClass('highlight') || term[event.currentTarget.termPosition].highlight){
+                            $(term[target.i].en + ".scroll").css("background-color", "")
+                            if($(this).hasClass('highlight') || term[target.i].highlight){
                                 if($(this).closest('pronounScope').length > 0){
-                                    $(this).closest('pronounScope').find(term[event.currentTarget.termPosition].en).each(function() {
+                                    $(this).closest('pronounScope').find(term[target.i].en).each(function() {
                                         $(this).css("background-color", "")
                                     })
                                 }else{
-                                    $(this).closest('padding').find(term[event.currentTarget.termPosition].en).each(function() {
+                                    $(this).closest('padding').find(term[target.i].en).each(function() {
                                         $(this).css("background-color", "")
                                     })
                                 }
@@ -80,27 +83,29 @@ function textReplace(path,mode) {
                 }
                 if(mode){
                     $(term[i].en).each((index, element) => {//高亮
-                        element.termPosition = i
+                        element.i = i
                         $(element).mouseover((event) => {
-                            for (var j in term[event.currentTarget.termPosition].part) {
-                                if(term[event.currentTarget.termPosition].part[j].termedPart){
-                                    if(term[event.currentTarget.termPosition].part[j].color){
-                                        $(element).find(term[event.currentTarget.termPosition].part[j].en).css("background-color", term[event.currentTarget.termPosition].part[j].color + '60')
-                                        $(term[event.currentTarget.termPosition].en + ".scroll").children(term[event.currentTarget.termPosition].part[j].en).css("background-color", term[event.currentTarget.termPosition].part[j].color + '60')
+                            var target = event.currentTarget
+                            for (var j in term[target.i].part) {
+                                if(term[target.i].part[j].termedPart){
+                                    if(term[target.i].part[j].color){
+                                        $(element).find(term[target.i].part[j].en).css("background-color", term[target.i].part[j].color + '60')
+                                        $(term[target.i].en + ".scroll").children(term[target.i].part[j].en).css("background-color", term[target.i].part[j].color + '60')
                                     }else{
-                                        $(element).find(term[event.currentTarget.termPosition].part[j].en).css("background-color", term[event.currentTarget.termPosition].color + '60')
-                                        $(term[event.currentTarget.termPosition].en + ".scroll").children(term[event.currentTarget.termPosition].part[j].en).css("background-color", term[event.currentTarget.termPosition].color + '60')
+                                        $(element).find(term[target.i].part[j].en).css("background-color", term[target.i].color + '60')
+                                        $(term[target.i].en + ".scroll").children(term[target.i].part[j].en).css("background-color", term[target.i].color + '60')
                                     }
                                 }else{
-                                    $(element).find(term[event.currentTarget.termPosition].part[j].en).css("background-color", term[event.currentTarget.termPosition].color)
-                                    $(term[event.currentTarget.termPosition].en + ".scroll").children(term[event.currentTarget.termPosition].part[j].en).css("background-color", term[event.currentTarget.termPosition].color)
+                                    $(element).find(term[target.i].part[j].en).css("background-color", term[target.i].color)
+                                    $(term[target.i].en + ".scroll").children(term[target.i].part[j].en).css("background-color", term[target.i].color)
                                 }
                             }
                         })
                         $(element).mouseout((event) => {
-                            for (var j in term[event.currentTarget.termPosition].part) {
-                                $(element).find(term[event.currentTarget.termPosition].part[j].en).css("background-color", "")
-                                $(term[event.currentTarget.termPosition].en + ".scroll").children(term[event.currentTarget.termPosition].part[j].en).css("background-color", "")
+                            var target = event.currentTarget
+                            for (var j in term[target.i].part) {
+                                $(element).find(term[target.i].part[j].en).css("background-color", "")
+                                $(term[target.i].en + ".scroll").children(term[target.i].part[j].en).css("background-color", "")
                             }
                         })
                     })
@@ -112,7 +117,8 @@ function textReplace(path,mode) {
                                     $("#example-tabs").foundation('selectTab', 'panel_term', 1);
                                     document.querySelectorAll(".scroll").forEach(
                                         scroll => {
-                                            if (scroll.outerHTML.startsWith("<" + term[event.currentTarget.termPosition].en.toLowerCase() + " ")) {
+                                            var target = event.currentTarget
+                                            if (scroll.outerHTML.startsWith("<" + term[target.i].en.toLowerCase() + " ")) {
                                                 if (!(scroll.classList.contains('fadeOnly'))) {
                                                     scroll.scrollIntoView({ behavior: 'smooth' });
                                                     $(scroll).fadeTo(200, 0).fadeTo(1000, 1)
@@ -127,17 +133,19 @@ function textReplace(path,mode) {
                     for (var j in term[i].part) {
                         if (term[i].part[j].termedPart){
                             $(term[i].part[j].en).each((index, element) => {
-                                element.termPartFatherPosition = i
-                                element.termPartPosition = j
+                                element.i = i
+                                element.j = j
                                 $(element).mouseover((event) => {
-                                    if(term[event.currentTarget.termPartFatherPosition].part[event.currentTarget.termPartPosition].color){
-                                        $(term[event.currentTarget.termPartFatherPosition].part[event.currentTarget.termPartPosition].en + ".scroll").css("background-color", term[event.currentTarget.termPartFatherPosition].part[j].color + '60')
+                                    var target = event.currentTarget
+                                    if(term[target.i].part[target.j].color){
+                                        $(term[target.i].part[target.j].en + ".scroll").css("background-color", term[target.i].part[j].color + '60')
                                     }else{
-                                        $(term[event.currentTarget.termPartFatherPosition].part[event.currentTarget.termPartPosition].en + ".scroll").css("background-color", term[event.currentTarget.termPartFatherPosition].color + '60')
+                                        $(term[target.i].part[target.j].en + ".scroll").css("background-color", term[target.i].color + '60')
                                     }
                                 })
                                 $(element).mouseout((event) => {
-                                    $(term[event.currentTarget.termPartFatherPosition].part[event.currentTarget.termPartPosition].en + ".scroll").css("background-color", "")
+                                    var target = event.currentTarget
+                                    $(term[target.i].part[target.j].en + ".scroll").css("background-color", "")
                                 })
                             })
                             document.querySelectorAll(term[i].part[j].en).forEach(//滚动
@@ -148,7 +156,8 @@ function textReplace(path,mode) {
                                             $("#example-tabs").foundation('selectTab', 'panel_term', 1);
                                             document.querySelectorAll(".scroll").forEach(
                                                 scroll => {
-                                                    if (scroll.outerHTML.startsWith("<" + term[event.currentTarget.termPartFatherPosition].part[event.currentTarget.termPartPosition].en.toLowerCase() + " ")) {
+                                                    var target = event.currentTarget
+                                                    if (scroll.outerHTML.startsWith("<" + term[target.i].part[target.j].en.toLowerCase() + " ")) {
                                                         if (!(scroll.classList.contains('fadeOnly'))) {
                                                             scroll.scrollIntoView({ behavior: 'smooth' });
                                                             $(scroll).fadeTo(200, 0).fadeTo(1000, 1)
