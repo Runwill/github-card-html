6bab4918b2339c57315061ec818555d2dfa66029
Runwill <1798273016@qq.com>
2026-01-30 00:46:20 +0800

    多余汉化删除

--- game/i18n/strings.js
+++ game/i18n/strings.js
@@ -1,6 +1,5 @@
 (function(){
     const zh = {
-        'game.round': '第 {n} 轮',
         'game.start': '开始游戏',
         'game.setup': '游戏设置',
         'game.setup.playerCount': '角色人数',
@@ -10,14 +9,10 @@
         'game.resume': '恢复游戏',
         'game.endTurn': '结束回合',
         'game.nextStep': '下一步',
-        'game.playCard': '使用/打出牌',
-        'game.endActing': '结束出牌',
-
-        'game.characterName': '角色名'
+        'game.playCard': '使用/打出牌'
     };
 
     const en = {
-        'game.round': 'Round {n}',
         'game.start': 'Start Game',
         'game.setup': 'Game Setup',
         'game.setup.playerCount': 'Player Count',
@@ -27,10 +22,7 @@
         'game.resume': 'Resume Game',
         'game.endTurn': 'End Turn',
         'game.nextStep': 'Next Step',
-        'game.playCard': 'Use/Play Card',
-        'game.endActing': 'End Acting',
-
-        'game.characterName': 'Character Name'
+        'game.playCard': 'Use/Play Card'
     };
 
     if (window.I18N_STRINGS) {
